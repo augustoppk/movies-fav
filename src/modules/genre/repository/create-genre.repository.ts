af728db71acde314402476d8b3b553c866a3ebe7
augustoppk <yzukke@gmail.com>
2025-11-24 01:01:51 -0300

    vamo arrumar essa poha amanha

--- src/modules/genre/repository/create-genre.repository.ts
+++ src/modules/genre/repository/create-genre.repository.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable prettier/prettier */
 import { Injectable } from "@nestjs/common";
 import { CreateGenreDto } from "../dto/create-genre.dto";
